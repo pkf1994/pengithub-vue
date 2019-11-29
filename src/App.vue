de1d4639c10630b951de6b9aa6493a70304a6214
Peng Kaifan <2811825214@qq.com>
2019-11-29 14:26:49 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
-  <div id="app">
+  <div id="app" :theme="currentTheme">
     <img alt="Vue logo" src="./assets/logo.png">
+    <div  @click="swithTheme">switch</div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input v-model.number="number" type="number" step="20">
+    <AnimatedNumber :value="number"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import {AnimatedNumber} from './components'
+/* eslint-disable */
+import {LIGHT_THEME,DARK_THEME} from "./constant/theme";
 
 export default {
   name: 'app',
+  data: () => {
+      return {
+          currentTheme: LIGHT_THEME,
+          number: 0
+      }
+  },
   components: {
-    HelloWorld
+    HelloWorld,
+    AnimatedNumber
+  },
+  methods: {
+    swithTheme(){
+      if(this.currentTheme == DARK_THEME) {
+        this.currentTheme = LIGHT_THEME;
+        return
+      }
+      this.currentTheme = DARK_THEME
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "scss/index";
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
+@include with-theme();
+
 </style>
---
+++ src/components/AnimatedNumber.vue
@@ -0,0 +1,37 @@
+<template>
+    <span>
+        {{numStr}}
+    </span>
+</template>
+
+<script>
+    import numeral from 'numeral'
+    export default {
+        props: {
+            value: {
+                default: 0,
+                required: true
+            }
+        },
+        data: () => ({
+            tweeningValue: 0
+        }),
+        computed: {
+            numStr: function(){
+                if(this.tweeningValue >= 1000) {
+                    return numeral(this.tweeningValue.toFixed(0)).format('0.0 a');
+                }
+                return numeral(this.tweeningValue.toFixed(0)).format('0 a');
+            }
+        },
+        watch: {
+            value: function(newValue){
+                TweenLite.to(this.$data, 0.5, { tweeningValue: newValue });
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
